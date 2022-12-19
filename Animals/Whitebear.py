871790b3415dc65e2510ee612103a11a4d9589ff
deadinside228pro <119436258+deadinside228pro@users.noreply.github.com>
2022-12-19 21:21:07 +0300

    Update Whitebear.py

--- Animals/Whitebear.py
+++ Animals/Whitebear.py
@@ -4,4 +4,4 @@ from animal import *
 class Whitebear(Animal):
     def __init__(self, name, age, howmanyfoodeat, size):
         super().__init__(name, age, howmanyfoodeat, size, voice='roar', typeOfFood=['мясо', 'рыба', 'ягоды'],
-                         squad='хищные', habitat='Тундра', species='белый медведь')
+                         squad='хищные', habitat='Тундра', species='белый медведь',foodVoice='хрумск *жуёт* хрямск grarh-rh')
