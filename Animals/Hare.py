5515fe57eae418f70f062255704cc7a68c4ebf14
deadinside228pro <119436258+deadinside228pro@users.noreply.github.com>
2022-12-19 21:22:58 +0300

    Update Hare.py

--- Animals/Hare.py
+++ Animals/Hare.py
@@ -4,4 +4,4 @@ from animal import *
 class Hare(Animal):
     def __init__(self, name, age, howmanyfoodeat, size):
         super().__init__(name, age, howmanyfoodeat, size, voice='fur-fur', typeOfFood=['капуста', 'трава', 'морковка'],
-                         squad='травоядное', habitat='Савана', species='заяц')
+                         squad='травоядное', habitat='Савана', species='заяц',foodVoice='хрум-хрум фыр-фыр хрум-хрум')
